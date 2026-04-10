ac46f0fd86ba38cb8bc98e39b2846a10cc8c6ac3
PratyushDhanuka <pratyushdhanuka@gmail.com>
2026-04-10 15:10:00 +0530

    adding count based policy implentation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from snapshot import InMemorySnapshotStore
+from snapshot import InMemorySnapshotStore, CountSnapshotPolicy
 from KVStore import KVStore
 
 
 def main():
     snap = InMemorySnapshotStore()
-    store = KVStore("node1", ["node2", "node3"], snapshot_store=snap)
+    policy = CountSnapshotPolicy(threshold=100)
+    store = KVStore("node1", ["node2", "node3"], snapshot_store=snap, snapshot_policy=policy)
     store.put("user:1", {"name": "Alice", "email": "alice@example.com"})
     print(store.get("user:1"))
 
